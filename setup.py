ff210a0240b0b4657bcfc3af546e96f126b7b48c
Nir Aides <nir@winpdb.org>
2010-01-31 17:39:35 +0200

    Move code to _jpc.py.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from distutils.core import setup
 
 setup(
     name = 'jpc',
-    version = '1.0.6',
+    version = '1.0.7',
     description = 'Fast JSON-RPC client/server module.',
     author = 'Nir Aides',
     author_email = 'nir@winpdb.org',

52b130ce4463133304ba95bfbcd0c2cb40ce38a6
Nir Aides <nir@winpdb.org>
2010-02-17 15:51:42 +0200

    docsw.

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ from distutils.core import setup
 
 setup(
     name = 'rfoo',
-    version = '1.0.7',
+    version = '1.0.8',
     description = 'Fast RPC client/server module.',
     author = 'Nir Aides',
     author_email = 'nir@winpdb.org',
